4d09c06abab554218f2b989c64c81b72a5de74c2
Tim Baumann <tim@timbaumann.info>
2016-09-14 23:09:01 +0200

    move CLAPACK dependency to 'deps'

--- build_lapack_emscripten.sh
+++ build_lapack_emscripten.sh
@@ -3,32 +3,33 @@
 set -e
 
 CC="emcc -Wall"
+CLAPACK="deps/CLAPACK"
 
 # compile f2c libraries
-for file in $( ls CLAPACK/F2CLIBS/libf2c | grep "\\.c\$" ); do
-  $CC -D INTEGER_STAR_8 -I CLAPACK/F2CLIBS/libf2c -c CLAPACK/F2CLIBS/libf2c/$file -o "build/`basename $file .c`.bc"
+for file in $( ls $CLAPACK/F2CLIBS/libf2c | grep "\\.c\$" ); do
+  $CC -D INTEGER_STAR_8 -I $CLAPACK/F2CLIBS/libf2c -c $CLAPACK/F2CLIBS/libf2c/$file -o "build/`basename $file .c`.bc"
 done
 
 # compile BLAS
-for file in $( ls CLAPACK/BLAS/SRC | grep "\\.c\$" ); do
-  $CC -I CLAPACK/INCLUDE -c CLAPACK/BLAS/SRC/$file -o "build/`basename $file .c`.bc"
+for file in $( ls $CLAPACK/BLAS/SRC | grep "\\.c\$" ); do
+  $CC -I $CLAPACK/INCLUDE -c $CLAPACK/BLAS/SRC/$file -o "build/`basename $file .c`.bc"
 done
 
 # compile CLAPACK
-for file in $( ls CLAPACK/SRC | grep "\\.c\$" ); do
-  $CC -I CLAPACK/INCLUDE -c CLAPACK/SRC/$file -o "build/`basename $file .c`.bc"
+for file in $( ls $CLAPACK/SRC | grep "\\.c\$" ); do
+  $CC -I $CLAPACK/INCLUDE -c $CLAPACK/SRC/$file -o "build/`basename $file .c`.bc"
 done
 
 # compile CLAPACK/INSTALL
-#for file in $( ls CLAPACK/INSTALL | grep "\\.c\$" ); do
-#  $CC -I CLAPACK/INCLUDE -c CLAPACK/INSTALL/$file -o "build/`basename $file .c`.bc"
+#for file in $( ls $CLAPACK/INSTALL | grep "\\.c\$" ); do
+#  $CC -I $CLAPACK/INCLUDE -c $CLAPACK/INSTALL/$file -o "build/`basename $file .c`.bc"
 #done
 
-$CC -I CLAPACK/INCLUDE -c CLAPACK/INSTALL/dlamch.c -o build/dlamch.bc
+$CC -I $CLAPACK/INCLUDE -c $CLAPACK/INSTALL/dlamch.c -o build/dlamch.bc
 
 rm build/main.bc
 
-$CC -I CLAPACK/INCLUDE -c jsapi.c -o build/jsapi.bc
+$CC -I $CLAPACK/INCLUDE -c jsapi.c -o build/jsapi.bc
 
 # build clapack.js
 # using `--llvm-lto 1` makes the JS file about 50% smaller
