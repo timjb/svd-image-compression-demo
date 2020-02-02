873285c0dd889564511e89e320ed7aae5d9283bc
Tim J. Baumann <tim@timbaumann.info>
2020-02-02 18:32:45 +0100

    Update gulp to v4

--- gulpfile.js
+++ gulpfile.js
@@ -6,37 +6,31 @@ var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var buffer = require('vinyl-buffer');
 
-gulp.task("default", [
-  "copy-react",
-  "copy-slick-assets",
-  "copy-nouislider-assets",
-  "compile-main-app",
-  "compile-web-worker"
-]);
-
-gulp.task("copy-react", function () {
+function copyReact () {
   return gulp.src([
     "node_modules/react/dist/react.min.js",
     "node_modules/react-dom/dist/react-dom.min.js"
   ]).pipe(gulp.dest("build"));
-});
+}
 
-gulp.task("copy-slick-assets", function () {
-  const copyCss =
-    gulp.src("node_modules/slick-carousel/slick/{slick,slick-theme}.css")
+function copySlickCss () {
+  return gulp.src("node_modules/slick-carousel/slick/{slick,slick-theme}.css")
     .pipe(gulp.dest("build"));
-  const copyFonts =
-    gulp.src("node_modules/slick-carousel/slick/fonts/slick.*")
-    .pipe(gulp.dest("build/fonts"));
-  return [copyCss, copyFonts];
-});
+}
 
-gulp.task("copy-nouislider-assets", function () {
+function copySlickFonts () {
+  return gulp.src("node_modules/slick-carousel/slick/fonts/slick.*")
+    .pipe(gulp.dest("build/fonts"));
+}
+
+const copySlickAssets = gulp.parallel(copySlickCss, copySlickFonts);
+
+function copyNoUiSliderAssets () {
   return gulp.src("node_modules/nouislider/distribute/nouislider.min.css")
     .pipe(gulp.dest("build"));
-});
+}
 
-gulp.task("compile-main-app", function () {
+function compileMainApp () {
   return browserify({
       basedir: 'src/main-app',
       debug: true,
@@ -53,9 +47,9 @@ gulp.task("compile-main-app", function () {
   .pipe(uglify())
   .pipe(sourcemaps.write('./'))
   .pipe(gulp.dest("build"));
-});
+}
 
-gulp.task("compile-web-worker", function () {
+function compileWebWorker () {
   return browserify({
       basedir: 'src/web-worker',
       debug: true,
@@ -71,4 +65,13 @@ gulp.task("compile-web-worker", function () {
   .pipe(uglify())
   .pipe(sourcemaps.write('./'))
   .pipe(gulp.dest("build"));
-});
+}
+
+// TODO: clean step
+exports.default = gulp.parallel(
+  copyReact,
+  copySlickAssets,
+  copyNoUiSliderAssets,
+  compileMainApp,
+  compileWebWorker
+);
